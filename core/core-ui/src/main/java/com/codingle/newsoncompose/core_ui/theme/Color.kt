3dc3b8ec08206262788dff4e7bfe7f6c57b934bd
irfan.pertadima <pertadimairfan@gmail.com>
2024-07-04 16:58:55 +0700

    create color pallete

--- core/core-ui/src/main/java/com/codingle/newsoncompose/core_ui/theme/Color.kt
+++ core/core-ui/src/main/java/com/codingle/newsoncompose/core_ui/theme/Color.kt
@@ -15,4 +15,31 @@ object Red {
 
 object Amber {
     val Amber100 = Color(0xFFFFF8E1)
+    val Amber500 = Color(0xFFFFC107)
+    val Amber800 = Color(0xFFFF6F00)
+}
+
+object Green {
+    val Green100 = Color(0xFFC8E6C9)
+    val Green500 = Color(0xFF4CAF50)
+    val Green800 = Color(0xFF2E7D32)
+}
+
+object Gray {
+    val Gray100 = Color(0xFFF5F5F5)
+    val Gray300 = Color(0xFFE0E0E0)
+    val Gray400 = Color(0xFFBDBDBD)
+    val Gray600 = Color(0xFF757575)
+    val Gray700 = Color(0xFF424242)
+    val Gray800 = Color(0xFF303030)
+}
+
+object BnW {
+    val Black = Color(0xFF000000)
+    val White = Color(0xFFFFFFFF)
+}
+
+object Error {
+    val ErrorRed = Color(0xFFD32F2F)
+    val ErrorContainerRed = Color(0xFFFFCDD2)
 }
--- core/core-ui/src/main/java/com/codingle/newsoncompose/core_ui/theme/Theme.kt
+++ core/core-ui/src/main/java/com/codingle/newsoncompose/core_ui/theme/Theme.kt
@@ -5,19 +5,103 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import com.codingle.newsoncompose.core_ui.theme.Amber.Amber100
+import com.codingle.newsoncompose.core_ui.theme.Amber.Amber500
+import com.codingle.newsoncompose.core_ui.theme.Amber.Amber800
+import com.codingle.newsoncompose.core_ui.theme.BnW.Black
+import com.codingle.newsoncompose.core_ui.theme.BnW.White
+import com.codingle.newsoncompose.core_ui.theme.Error.ErrorContainerRed
+import com.codingle.newsoncompose.core_ui.theme.Error.ErrorRed
+import com.codingle.newsoncompose.core_ui.theme.Gray.Gray100
+import com.codingle.newsoncompose.core_ui.theme.Gray.Gray300
+import com.codingle.newsoncompose.core_ui.theme.Gray.Gray400
+import com.codingle.newsoncompose.core_ui.theme.Gray.Gray600
+import com.codingle.newsoncompose.core_ui.theme.Gray.Gray700
+import com.codingle.newsoncompose.core_ui.theme.Gray.Gray800
+import com.codingle.newsoncompose.core_ui.theme.Green.Green100
+import com.codingle.newsoncompose.core_ui.theme.Green.Green500
+import com.codingle.newsoncompose.core_ui.theme.Green.Green800
+import com.codingle.newsoncompose.core_ui.theme.Red.Red100
 import com.codingle.newsoncompose.core_ui.theme.Red.Red200
 import com.codingle.newsoncompose.core_ui.theme.Red.Red700
+import com.codingle.newsoncompose.core_ui.theme.Red.Red900
 
 val DarkColorScheme = darkColorScheme(
     primary = Red700,
-    secondary = Red200,
-    tertiary = Pink80
+    onPrimary = White,
+    primaryContainer = Red900,
+    onPrimaryContainer = Red100,
+    inversePrimary = Red200,
+    secondary = Amber500,
+    onSecondary = Black,
+    secondaryContainer = Amber800,
+    onSecondaryContainer = Amber100,
+    tertiary = Green500,
+    onTertiary = White,
+    tertiaryContainer = Green800,
+    onTertiaryContainer = Green100,
+    background = Gray800,
+    onBackground = White,
+    surface = Gray800,
+    onSurface = White,
+    surfaceVariant = Gray800,
+    onSurfaceVariant = Gray400,
+    surfaceTint = Red700,
+    inverseSurface = White,
+    inverseOnSurface = Black,
+    error = ErrorRed,
+    onError = White,
+    errorContainer = Red900,
+    onErrorContainer = Red100,
+    outline = Gray600,
+    outlineVariant = Gray700,
+    scrim = Black,
+    surfaceBright = Gray800,
+    surfaceDim = Gray800,
+    surfaceContainer = Gray800,
+    surfaceContainerHigh = Gray700,
+    surfaceContainerHighest = Gray600,
+    surfaceContainerLow = Gray800,
+    surfaceContainerLowest = Gray800
 )
 
 val LightColorScheme = lightColorScheme(
     primary = Red700,
-    secondary = Red200,
-    tertiary = Pink40
+    onPrimary = White,
+    primaryContainer = Red100,
+    onPrimaryContainer = Red900,
+    inversePrimary = Red200,
+    secondary = Amber500,
+    onSecondary = Black,
+    secondaryContainer = Amber100,
+    onSecondaryContainer = Amber800,
+    tertiary = Green500,
+    onTertiary = White,
+    tertiaryContainer = Green100,
+    onTertiaryContainer = Green800,
+    background = White,
+    onBackground = Black,
+    surface = White,
+    onSurface = Black,
+    surfaceVariant = Gray100,
+    onSurfaceVariant = Gray600,
+    surfaceTint = Red700,
+    inverseSurface = Gray800,
+    inverseOnSurface = White,
+    error = ErrorRed,
+    onError = White,
+    errorContainer = ErrorContainerRed,
+    onErrorContainer = Red900,
+    outline = Gray400,
+    outlineVariant = Gray300,
+    scrim = Black,
+    surfaceBright = White,
+    surfaceDim = Gray100,
+    surfaceContainer = Gray100,
+    surfaceContainerHigh = Gray300,
+    surfaceContainerHighest = Gray400,
+    surfaceContainerLow = Gray100,
+    surfaceContainerLowest = White
 )
 
 @Composable
