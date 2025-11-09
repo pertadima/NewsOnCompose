bb9d80e88cb1b4d4015d995ee6b66b2ec53c8bd8
irfan.pertadima <pertadimairfan@gmail.com>
2025-11-09 19:11:16 +0700

    fix failed detekt

--- core/core/src/main/java/com/codingle/newsoncompose/core/di/CoreModule.kt
+++ core/core/src/main/java/com/codingle/newsoncompose/core/di/CoreModule.kt
@@ -1,11 +1,9 @@
 package com.codingle.newsoncompose.core.di
 
-import android.content.Context
 import com.codingle.newsoncompose.core.cache.ImageCacheHelper
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -14,5 +12,5 @@ import javax.inject.Singleton
 class CoreModule {
     @Singleton
     @Provides
-    fun providesImageCacheHelper(@ApplicationContext context: Context) = ImageCacheHelper()
+    fun providesImageCacheHelper() = ImageCacheHelper()
 }
